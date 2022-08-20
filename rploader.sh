9b2af371d9e251b10a22720e487f1b47fe4f8c5b
Kazagumo <13860126292@163.com>
2022-08-21 00:49:45 +0800

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -345,7 +345,7 @@ function downloadextractor() {
 
         mkdir /${workdir}/auxfiles/extractor
 
-        for file in usr/lib/libcurl.so.4 usr/lib/libmbedcrypto.so.5 usr/lib/libmbedtls.so.13 usr/lib/libmbedx509.so.1 usr/lib/libmsgpackc.so.2 usr/lib/libsodium.so usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 usr/syno/bin/scemd; do
+        for file in ${workdir}/tools/libcurl.so.4 usr/lib/libmbedcrypto.so.5 usr/lib/libmbedtls.so.13 usr/lib/libmbedx509.so.1 usr/lib/libmsgpackc.so.2 usr/lib/libsodium.so usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 usr/syno/bin/scemd; do
             echo "Copying $file to /${workdir}/auxfiles"
             cp $file /${workdir}/auxfiles/extractor
         done
